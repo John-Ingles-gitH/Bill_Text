00f1be3d5a0e68d37ed18350fb487afb06fa5bbe
John Ingles <john.kenneth.ingles@gmail.com>
2019-01-05 14:45:37 -0500

    Add error handing for pages_to_get parameter

--- CongressScrape.py
+++ CongressScrape.py
@@ -4,15 +4,24 @@ import time
 import re
 headers = {'User-Agent': 'Mozilla/5.0'}
 
-def get_link_list(page_link, last_page_num):
-	#Returns a list of urls, one for each Bill
-	if last_page_num == 'All':
-		page_response = req.get(page_link, headers=headers, timeout=5)
-		page_content = BeautifulSoup(page_response.content, "html.parser")
-		last_page = page_content.find('a',attrs={'class':'last'})
-		last_page_link = last_page.get('href')
-		last_page_num = re.sub('.*?([0-9]*)$',r'\1',last_page_link)
-		time.sleep(2)#needed to follow congress.gov's crawl limit
+def get_link_list(page_link, pages_to_get):
+	#Returns a list of urls, one for each Bill/Resolution on a search results page of congress.gov
+	
+	page_response = req.get(page_link, headers=headers, timeout=5)
+	page_content = BeautifulSoup(page_response.content, "html.parser")
+	last_page = page_content.find('a',attrs={'class':'last'}).get('href')
+	last_page_num = int(re.sub('.*?([0-9]*)$',r'\1',last_page))
+	time.sleep(2)#needed to follow congress.gov's crawl limit
+	
+	#Ensure pages_to_get isn't less than 1 or more than number of available pages
+	if pages_to_get == "All":
+		pages_to_get = last_page_num
+	if pages_to_get <1:
+		raise Exception('pages_to_get should not be less than 1')
+	if pages_to_get > last_page_num:
+		raise Exception('pages_to_get should not exceed '+str(last_page_num))
+	
+	
 		
 	def get_next_link():
 		#Convert page link to next page link
@@ -21,9 +30,9 @@ def get_link_list(page_link, last_page_num):
 		y=last + 1
 		return page_link[:-1]+str(y)
 
-	linkList= []
+	link_list= []
 	counter	= 1
-	while(int(re.sub('.*?([0-9]*)$',r'\1', page_link))!=last_page_num+1):
+	while(int(re.sub('.*?([0-9]*)$',r'\1', page_link))!=pages_to_get+1):
 
 		page_response = req.get(page_link, headers=headers, timeout=5)
 		page_content = BeautifulSoup(page_response.content, "html.parser")	
@@ -32,17 +41,17 @@ def get_link_list(page_link, last_page_num):
 
 		for elem in elems:
 			span = elem.find('span', attrs={'class':'result-heading'})
-			linkList.append(span.a.get('href'))
+			link_list.append(span.a.get('href'))
 
 		page_link = get_next_link()
 		print(counter)
 		counter+=1
 		time.sleep(2)#needed to follow congress.gov's crawl limit
 
-	return linkList
+	return link_list
 
 	
-linkList = get_link_list('https://www.congress.gov/search?q={%22source%22%3A%22legislation%22}&page=1', 1)
+link_list = get_link_list('https://www.congress.gov/search?q={%22source%22%3A%22legislation%22}&page=1',3)
 
-for n in range(len(linkList)):
-	print(linkList[n])
+for n in range(len(link_list)):
+	print(link_list[n])
