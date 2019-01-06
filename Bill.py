b6da6abb214709359f226432a2085d8c00f22c53
John Ingles <john.kenneth.ingles@gmail.com>
2019-01-05 23:59:05 -0500

    Fix Typos

--- Bill.py
+++ Bill.py
@@ -1,7 +1,7 @@
 class Bill:
 	'''Contains all the information about a congressional bill'''
-	def __init__(self, name, years, congress_num, sponser, sponsers_party,
-				num_cosponsers, bill_type, bill_link, path_to_text):
+	def __init__(self, name, years, congress_num, sponsor, sponsors_party,
+				num_cosponsors, bill_type, bill_link, path_to_text):
 		
 		'''
 		
@@ -9,9 +9,9 @@ class Bill:
 			name           (str): Name of the Bill
 			years          (str): 2 year congressional session
 			congress_num   (int): Congress Number
-			sponser        (str): Name of person who sponsered the Bill
-			sponsers_party (str): Sponser's political party
-			num_cosponsers (str): Number of cosponsers
+			sponsor        (str): Name of person who sponsored the Bill
+			sponsors_party (str): Sponsor's political party
+			num_cosponsors (str): Number of cosponsors
 			bill_type      (str): Bill Type (Bill, Resolution, Joint Resolution, etc.)
 			bill_link      (str): Url to bill
 			path_to_text   (str): file path to saved copy of Bill Text
@@ -20,12 +20,12 @@ class Bill:
 		self.name = name
 		self.years = years
 		self.congress_num = congress_num
-		self.sponser = sponser
-		self.sponsers_party = sponsers_party
-		self.num_cosponsers = num_cosponsers
+		self.sponseor = sponsor
+		self.sponsors_party = sponsors_party
+		self.num_cosponsors = num_cosponsors
 		self.bill_type = bill_type
 		self.bill_link = bill_link
-		self.path_to_text=path_to_text
+		self.path_to_text = path_to_text
 	
 	def text(self):
 		#return text of bill from file
